687d3d0e49cd8f489eec3ef1546d9b3d9cd2962a
KR-ISCOD3 <krisjenxz@example.com>
2025-01-17 23:51:03 +0700

    newUpdate

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGOOSE_URL);
+    await mongoose.connect(process.env.MONGOOSE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- index.js
+++ index.js
@@ -14,13 +14,8 @@ const app = express();
 const allowedOrigins = ["http://localhost:5174","http://localhost:5173", "https://food-project-frontend-phi.vercel.app"]; 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
