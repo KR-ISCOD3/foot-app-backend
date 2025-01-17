ab58f94c0d51852859496fe1db63df3d42dab322
KR-ISCOD3 <krisjenxz@example.com>
2025-01-17 12:01:16 +0700

    update-googleId

--- model/authModel.js
+++ model/authModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  googleId: { type: String, unique: true, required: true },
+  googleId: { type: String, unique: true},
   name: { type: String, required: true },
   email: { type: String, unique: true, sparse: true },
   avatar: { type: String },
