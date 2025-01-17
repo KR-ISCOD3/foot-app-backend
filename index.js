df0a2d6a273f234ce0cbeff6f54fb357ea3ddca6
KR-ISCOD3 <krisjenxz@example.com>
2025-01-18 01:16:53 +0700

    neworigin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 connectDB();
 // Middleware
-const allowedOrigins = ["http://localhost:5174","http://localhost:5173", "https://food-project-frontend-phi.vercel.app"]; 
+const allowedOrigins = ["http://localhost:5174","http://localhost:5173", "https://food-app-rupp.vercel.app"]; 
 app.use(
   cors({
     origin: allowedOrigins,
